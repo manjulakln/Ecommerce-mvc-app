26e8925ad315e04fa88faec918e629f48341aa5a
pc <pc@DESKTOP-DICHVFG>
2023-02-02 11:36:12 +0530

    Add project files.

---
+++ Ecommercemvcapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ecommercemvcapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
